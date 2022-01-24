4c963f2272f6719bd7999c5c6b11884ac0f8c1ac
Mayank <goyalmayank522@gmail.com>
2022-01-24 18:13:52 +0530

    mysql bug fixes

--- saved_form.php
+++ saved_form.php
@@ -1,21 +1,38 @@
-<?php
-	define("hostname", "localhost");
-	define("username", "root");
-	define("password", "");
-	define("dbname", "contact_us_db");
-	$conn = mysqli_connect(hostname, username, password, dbname) or die(mysqli_error());
-	if ( isset($_POST['submit']) ) {
-		$name = mysqli_real_escape_string( $conn, $_POST['name'] );
-		$email = mysqli_real_escape_string( $conn, $_POST['email'] );
-		$phone = mysqli_real_escape_string( $conn, $_POST['contact'] );
-		$comment = mysqli_real_escape_string( $conn, $_POST['text'] );
-		$insert_query = "INSERT INTO contactus (name, email, contact, comments) VALUES ('$name', '$email', '$phone', '$comment')";
-		if ( mysqli_query($conn, $insert_query) ) {
-			echo "Thank you for your comments!";
-		} else {
-			die('Some Error occured: '.mysqli_error($conn));
-		}
-	}
-	mysqli_close($conn);
+<!DOCTYPE html>
+<html>
 
-?>
+<head>
+    <title>Success</title>
+</head>
+
+<body>
+    <center>
+        <?php
+        define("hostname", "localhost");
+        define("username", "example_user");
+        define("password", "password");
+        define("dbname", "contact_us_db");
+        $conn = mysqli_connect(hostname, username, password, dbname) or die('Error: ' .mysqli_error());
+        if ( isset($_POST['submit']) ) {
+                $name = mysqli_real_escape_string( $conn, $_POST['name'] );
+                $email = mysqli_real_escape_string( $conn, $_POST['email'] );
+                $phone = mysqli_real_escape_string( $conn, $_POST['contact'] );
+                $comment = mysqli_real_escape_string( $conn, $_POST['text'] );
+                $insert_query = "INSERT INTO contact_us (name, email, contact, comments) VALUES ('$name', '$email', '$phone', '$comment')";
+                if ( mysqli_query($conn, $insert_query) ) {
+                        echo "Thank you for your comments!";
+                } else {
+                        die('Some Error occured: ' .mysqli_error());
+                }
+        }
+        mysqli_close($conn);
+
+        ?>
+        <form action="index.html" method="GET">
+          <input type="submit" class="btn btn-primary" value="Back"/>
+        </form>
+    </center>
+</body>
+
+</html>
+~             
